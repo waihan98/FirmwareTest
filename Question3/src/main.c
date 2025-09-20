cd65a42be1f81b2e6a9f54e0cda071758a2ce3a2
Wai Han <waihan98@DESKTOP-KFGS487.localdomain>
2025-09-20 15:09:13 +0800

    Q3

---
+++ Question3/src/main.c
@@ -0,0 +1,39 @@
+#include <stdint.h>
+#include <stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+enum LED{
+    GREEN,
+    RED,
+};
+enum PUSH_BUTTON{
+    PB_1,
+    PB_2,
+};
+
+int set_led(enum LED led_number, uint16_t interval);
+int set_push_button(enum PUSH_BUTTON push_button, void (*callback_func)(void));
+void led_app(void);
+void push_button_app(void);
+
+int main (void)
+{
+    uint8_t ledCol;
+    uint8_t selDone = FALSE;
+    while(selDone != TRUE)                  //Loop to make sure person select available color
+    {
+        printf("Select LED Colour \n");
+        printf("Green [0]\t Red[1]\n");
+        scanf("%hhu", &ledCol);
+        if(ledCol >1)         //Ensure only 1 or 0
+        {
+            printf("Choose only 1 or 0\n");
+        }
+        else selDone = TRUE;
+    }
+    return 1;
+}
+
+
